311df140aad61f4f4c5311cc203293ff50575230
qklrise <qklrise@gmail.com>
2025-02-28 14:43:31 +0900

    인형이 바라보는 방향에 있는 오브젝트와만 상호작용 가능하게 수정

---
+++ Assets/Button.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Button : StateMachineBehaviour
+{
+    public string paraName = "On";
+    public string triggerName = "Using";
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // 상태에 진입할 때 호출
+    {
+        animator.SetBool(paraName, true);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // 상태를 빠져나갈 때 호출
+    {
+        if (triggerName != string.Empty) animator.ResetTrigger(triggerName);
+
+        animator.SetBool(paraName, false);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
